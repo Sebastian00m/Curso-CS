15a17bd38f323a97c79de85752280c55fd6e121d
Sebastian.00m <montanomauro369@gmail.com>
2026-01-04 02:11:55 -0300

    Generics e Interface

---
+++ Generics/Program.cs
@@ -0,0 +1,109 @@
+﻿//Generic : basicamente nos ayuda a reutilizar codigo
+using System.ComponentModel;
+using System.Globalization;
+
+MyList numbers = new MyList(10);
+numbers.Add(5);
+numbers.Add(3);
+numbers.Add(2);
+
+//ejemplo con Generic
+MyListGeneric<int> numbersGeneric = new MyListGeneric<int>(10);
+numbersGeneric.Add(3);
+numbersGeneric.Add(5);
+numbersGeneric.Add(4);
+numbersGeneric.Add(7);
+
+MyListGeneric<string> stringsGeneric = new MyListGeneric<string>(10);
+stringsGeneric.Add("Mauro");
+stringsGeneric.Add("Franco");
+stringsGeneric.Add("Lucas");
+stringsGeneric.Add("Sebastian");
+
+MyListGeneric<People> people = new MyListGeneric<People>(10);
+people.Add(new People() { Name = "Mauro", Country = "Argentina" });
+people.Add(new People() { Name = "Franco", Country = "Argentina" });
+
+
+
+Console.WriteLine(numbersGeneric.GetElement(0));
+Console.WriteLine(numbersGeneric.GetElement(2));
+Console.WriteLine(numbersGeneric.GetElement(4));//devuelve 0 por default, fuera de rango
+Console.WriteLine(stringsGeneric.GetElement(0));
+Console.WriteLine(stringsGeneric.GetElement(3));
+Console.WriteLine(stringsGeneric.GetElement(4));//devuelve null por default, fuera de rango
+Console.WriteLine(numbersGeneric.GetString());
+Console.WriteLine(stringsGeneric.GetString());
+Console.WriteLine(people.GetString());
+
+public class MyList //clase para alamcenar n elementos y valida que no excedan un limite
+{ 
+    private int[] _elements;
+    private int _index = 0;
+
+    public MyList(int n)//constructor de mi lista
+    {
+        _elements = new int[n];
+    }
+
+    public void Add(int e)
+    {
+        if (_index < _elements.Length)
+        {
+            _elements[_index] = e;
+            _index++;
+        }
+    }
+}
+
+public class MyListGeneric<T>
+{
+    private T[] _elements;
+    private int _index = 0;
+
+    public MyListGeneric(int n)
+    {
+        _elements = new T[n];
+    }
+
+    public void Add(T e)
+    {
+        if (_index < _elements.Length)
+        {
+            _elements[_index] = e;
+            _index++;
+        }
+    }
+
+    public T GetElement(int i)
+    {
+        if (i <= _index && i >= 0)
+        {
+            return _elements[i];
+        }
+        return default(T);//el valor por defecto de un objeto es el null
+    }
+
+    public string GetString()
+    {
+        int i = 0;
+        string result = "";
+        while (i < _index)
+        {
+            result += _elements[i].ToString() + "|";
+            i++;
+        }
+        return result;
+    }
+}
+
+public class People
+{ 
+    public string Name { get; set; }
+    public string Country { get; set; }
+
+    public override string ToString()
+    {
+        return $"Nombre: {Name}, Pais: {Country}";
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,93 @@
+﻿//INTERFACE :
+//ES un contrato que debe complir una clase, es decir que si quiero implementar una interfaz debo cumplir con los metodos y propiedades que este exige
+
+//Fundamentalmente sirven para categorizar las clases y obligarlas a cumplir con ciertos metodos y propiedades
+//las interfaces se utilizan para "obligar a utilizar ciertos metodos y propiedades"
+//ayuda mucho a la convencion de codigo para los programadores
+
+Shark[] sharks = new Shark[]
+{
+    new Shark("tibu1", 45),
+    new Shark("tibu2", 55),
+    new Shark("tibu3", 65)
+};
+
+//como crear un array de Sirenas sin utilizar el tipo/objetos Siren, puedo hacerlo directamente de la interfaz
+IFish[] fishs = new IFish[]
+{
+    new Siren(100),
+    new Shark("Tiburoncin", 100)//tanto Siren como Shark cumplen con el contrato de IFish
+};
+
+ShowFish(sharks);
+ShowAnimal(sharks);
+ShowFish(fishs);//ya que Shark y Siren tienen implementado el metodo ShowSwim porque cumplen con la interface IFish
+
+
+
+// Defino métodos que puedan implementar las interfaces
+void ShowFish(IFish[] fishs)
+{
+    Console.WriteLine("-- Mostramos los peces de la interface IFish --");
+    int i = 0;
+    while (i < fishs.Length)
+    {
+        Console.WriteLine(fishs[i].Swim());
+        i++;
+    }
+}
+//defino un metodo en main para poder recorrer la interface IAnimal
+void ShowAnimal(IAnimal[] animals)
+{
+    Console.WriteLine("-- Mostramos los animales de la interface IAnimal --");
+    int i = 0;
+    while (i < animals.Length)
+    {
+        Console.WriteLine(animals[i].Name);
+        i++;
+    }
+}
+
+
+public class Shark : IAnimal, IFish
+{
+    public string Name { get; set; }
+
+    public int Velocidad { get; set; }
+
+    public Shark(string name, int velocidad)
+    {
+        this.Name = name;
+        this.Velocidad = velocidad;
+    }
+    public string Swim()
+    {
+        return $"{Name} nada  a {Velocidad} km/h";
+    }
+}
+
+public class Siren : IFish
+{
+    public int Velocidad { get; set; }
+
+    public Siren(int velocidad)
+    {
+        this.Velocidad = velocidad;
+    }
+    public string Swim()
+    {
+        return $"La sirena nada a {Velocidad} km/h";
+    }
+}
+public interface IAnimal
+{
+    public string Name { get; set; }
+    //si qeures utilizar con la interfaz IAnimal debes implementar esta propiedad en tu clase
+}
+
+public interface IFish
+{
+    public int Velocidad { get; set; }
+    public string Swim();
+    //si queres utilizar la interface IFish, debes implemtar la prop Velocidad y el metodo Swim
+}
